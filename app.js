d025e3233734cbe9c4adec4ae7ac5bddfe096676
Tulio <lucasdnd@gmail.com>
2016-03-22 16:11:16 -0300

    Page layout and api request

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const apiUrl = 'http://localhost:5000/image';
 // Get!
 var get = new XMLHttpRequest();
 get.onload = function() {
-  const image_url = get.response
-  document.getElementsByTagName('html')[0].style.backgroundImage = 'url("' + image_url + '")';
+  const result = JSON.parse(get.response);
+  
+  // Apply the data on the screen
+  document.getElementsByTagName('html')[0].style.backgroundImage = 'url("' + result.url_hd + '")';
+  const author = result.user;
+  const title = result.title;
+  const imageId = result.id;
+  document.getElementById('image-title').innerHTML = title;
+  document.getElementById('image-author').innerHTML = 'Image by ' + author;
+  document.getElementById('image-link').href = 'http://astrobin.com/' + imageId;
 }
 get.open('GET', apiUrl, true);
-get.send();
