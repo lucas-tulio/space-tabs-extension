eb2caf735bb42ab5f8a600c9f8492c8fa0f35258
Tulio <lucasdnd@gmail.com>
2016-03-23 14:51:08 -0300

    Pointing to the correct server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // API
-const apiUrl = 'http://localhost:5000/image';
+const apiUrl = 'http://autopresentr.co:8001/image';
 
 // Get!
 var get = new XMLHttpRequest();
@@ -7,13 +7,20 @@ get.onload = function() {
   const result = JSON.parse(get.response);
   
   // Apply the data on the screen
-  document.getElementsByTagName('html')[0].style.backgroundImage = 'url("' + result.url_hd + '")';
+  const imageUrl = result.url_hd;
   const author = result.user;
   const title = result.title;
   const imageId = result.id;
+  
+  document.getElementsByTagName('html')[0].style.backgroundImage = 'url("' + imageUrl + '")';
   document.getElementById('image-title').innerHTML = title;
   document.getElementById('image-author').innerHTML = 'Image by ' + author;
   document.getElementById('image-link').href = 'http://astrobin.com/' + imageId;
 }
+
+get.onerror = function() {
+  document.getElementById('image-author').innerHTML = 'Error loading image';
+}
+
 get.open('GET', apiUrl, true);
 get.send();
